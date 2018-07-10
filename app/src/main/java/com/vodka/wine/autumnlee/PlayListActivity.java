8019ff04f92e680097aae70275052aeb1c46c431
Nguyen Xuan Khoa <xuankhoa.94@gmail.com>
2018-07-11 00:44:21 +0700

    read all mp3 file from storage

--- app/src/main/java/com/vodka/wine/autumnlee/PlayListActivity.java
+++ app/src/main/java/com/vodka/wine/autumnlee/PlayListActivity.java
@@ -1,81 +1,204 @@
 package com.vodka.wine.autumnlee;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
 
-import android.app.ListActivity;
-import android.content.Intent;
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
-public class PlayListActivity extends ListActivity {
-    // Songs list
-    public ArrayList<HashMap<String, String>> songsList = new ArrayList<>();
+public class PlayListActivity extends AppCompatActivity {
+    Context context;
+    public static final int RUNTIME_PERMISSION_CODE = 7;
+
+    String[] ListElements = new String[] { };
+
+    ListView listView;
+
+    List<String> ListElementsArrayList ;
+
+    ArrayAdapter<String> adapter ;
+
+    ContentResolver contentResolver;
+
+    Cursor cursor;
+
+    Uri uri;
+
+    Button button;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.playlist);
 
-        ArrayList<HashMap<String, String>> songsListData = new ArrayList<>();
+        listView = findViewById(R.id.listView1);
 
-        SongsManager plm = new SongsManager();
-        // get all songs from sdcard
-        ArrayList<HashMap<String,String>> songsList = plm.getPlayList(Environment.getDataDirectory().toString());
-        Log.i("xxx", "onCreate: songList " + songsList + " path: " + Environment.getDataDirectory().toString());
-        if(songsList!=null){
-            for(int i=0;i<songsList.size();i++){
-                String fileName=songsList.get(i).get("songTitle");
-                String filePath=songsList.get(i).get("songPath");
-                //here you will get list of file name and file path that present in your device
-                Log.i("xxx "," name ="+fileName +" path = "+filePath);
-            }
-        }
+        button = findViewById(R.id.button);
 
-        // looping through playlist
-        for (int i = 0; i < songsList.size(); i++) {
-            // creating new HashMap
-            HashMap<String, String> song = songsList.get(i);
+        context = getApplicationContext();
 
-            // adding HashList to ArrayList
-            songsListData.add(song);
-        }
+        ListElementsArrayList = new ArrayList<>(Arrays.asList(ListElements));
 
-        // Adding menuItems to ListView
-        ListAdapter adapter = new SimpleAdapter(this, songsListData,
-                R.layout.playlist, new String[] { "songTitle" }, new int[] {
-                R.id.songTitle});
+        adapter = new ArrayAdapter<>
+                (PlayListActivity.this, android.R.layout.simple_list_item_1, ListElementsArrayList);
 
-        setListAdapter(adapter);
+        // Requesting run time permission for Read External Storage.
+        AndroidRuntimePermission();
 
-        // selecting single ListView item
-        ListView lv = getListView();
-        // listening to single listitem click
-        lv.setOnItemClickListener(new OnItemClickListener() {
-
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                // getting listitem index
-//                int songIndex = position;
+            public void onClick(View view) {
 
-                // Starting new intent
-                Intent in = new Intent(getApplicationContext(),
-                        MainActivity.class);
-                // Sending songIndex to PlayerActivity
-                in.putExtra("songIndex", position);
-                setResult(100, in);
-                // Closing PlayListView
-                finish();
+                GetAllMediaMp3Files();
+
+                listView.setAdapter(adapter);
+
             }
         });
 
+        // ListView on item selected listener.
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // TODO Auto-generated method stub
+                // Showing ListView Item Click Value using Toast.
+
+                Toast.makeText(PlayListActivity.this,parent.getAdapter().getItem(position).toString(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+
+    public void GetAllMediaMp3Files(){
+
+        contentResolver = context.getContentResolver();
+
+        uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+
+        cursor = contentResolver.query(
+                uri, // Uri
+                null,
+                null,
+                null,
+                null
+        );
+
+        if (cursor == null) {
+
+            Toast.makeText(PlayListActivity.this,"Something Went Wrong.", Toast.LENGTH_LONG);
+
+        } else if (!cursor.moveToFirst()) {
+
+            Toast.makeText(PlayListActivity.this,"No Music Found on SD Card.", Toast.LENGTH_LONG);
+
+        }
+        else {
+
+            int Title = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+
+            //Getting Song ID From Cursor.
+            //int id = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
+
+            do {
+
+                // You can also get the Song ID using cursor.getLong(id).
+                //long SongID = cursor.getLong(id);
+
+                String SongTitle = cursor.getString(Title);
+
+                // Adding Media File Names to ListElementsArrayList.
+                ListElementsArrayList.add(SongTitle);
+
+            } while (cursor.moveToNext());
+        }
+    }
+
+    // Creating Runtime permission function.
+    public void AndroidRuntimePermission(){
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+                    AlertDialog.Builder alert_builder = new AlertDialog.Builder(PlayListActivity.this);
+                    alert_builder.setMessage("External Storage Permission is Required.");
+                    alert_builder.setTitle("Please Grant Permission.");
+                    alert_builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            ActivityCompat.requestPermissions(
+                                    PlayListActivity.this,
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    RUNTIME_PERMISSION_CODE
+
+                            );
+                        }
+                    });
+
+                    alert_builder.setNeutralButton("Cancel",null);
+
+                    AlertDialog dialog = alert_builder.create();
+
+                    dialog.show();
+
+                }
+                else {
+
+                    ActivityCompat.requestPermissions(
+                            PlayListActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            RUNTIME_PERMISSION_CODE
+                    );
+                }
+            } else {
+                Log.i("xuankhoa94", "not permision");
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+
+        switch(requestCode){
+
+            case RUNTIME_PERMISSION_CODE:{
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                }
+                else {
+
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/vodka/wine/autumnlee/SongsManager.java
+++ app/src/main/java/com/vodka/wine/autumnlee/SongsManager.java
@@ -1,19 +1,59 @@
 package com.vodka.wine.autumnlee;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
 public class SongsManager{
 
+    private Context context;
+
+    private List<String> ListElementsArrayList;
+//    List<String> ListElementsArrayList;
     // Constructor
     public SongsManager(){
 
     }
 
+    public void getAllMp3Files() {
+        ContentResolver contentResolver;
+        contentResolver = context.getContentResolver();
+        Uri uri = MediaStore.Audio.Media.INTERNAL_CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        if(cursor == null) {
+            Log.i("xxx", "getAllMp3Files: somethings went wrong");
+        } else if (!cursor.moveToFirst()) {
+            Log.i("xxx", "no mp3 file");
+        } else {
+            int Title = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+
+            //Getting Song ID From Cursor.
+            //int id = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
+
+            do {
+
+                // You can also get the Song ID using cursor.getLong(id).
+                //long SongID = cursor.getLong(id);
+
+                String SongTitle = cursor.getString(Title);
+
+                // Adding Media File Names to ListElementsArrayList.
+
+                ListElementsArrayList.add(SongTitle);
+
+            } while (cursor.moveToNext());
+        }
+    }
     ArrayList<HashMap<String,String>> getPlayList(String rootPath) {
         ArrayList<HashMap<String,String>> fileList = new ArrayList<>();
 
